fff6ecc1f1905d3ad842bc1d5f4ac8650451f3c5
Ivy Cagaanan <107980264+Lothany@users.noreply.github.com>
2023-10-07 01:17:26 +0800

    added pages for college, course and student

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/student')
 def student():
-    return "<p>student page</p>"
+    return render_template("student.html")
 
 @auth.route('/course')
 def course():
-    return "<p>course page</p>"
+    return render_template("course.html")
 
 @auth.route('/college')
 def college():
-    return "<p>college page</p>"
+    return render_template("college.html")
     
