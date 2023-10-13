4725b0b39d0d03b814c383221cf284dc8a7fd2d0
Ivy Cagaanan <107980264+Lothany@users.noreply.github.com>
2023-10-13 23:15:03 +0800

    included MYSQL_PASSWORD that was accidentally deleted

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_mysql_connector import MySQL
 
+mysql = MySQL(app)
+
 def create_app():
     app = Flask(__name__)
     
     app.config['SECRET_KEY'] = 'password'
     app.config['MYSQL_USER'] = "root"
+    app.config['MYSQL_PASSWORD'] = "password123"
     app.config['MYSQL_DATABASE'] = "test"
     app.config['MYSQL_HOST'] = "localhost"
     
-    mysql = MySQL(app)
+    mysql.init_app(app)
 
     from .auth import auth
     from .view import view
