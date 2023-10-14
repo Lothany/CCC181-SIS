2d19d0e4d659cd352b89b2708a5e2164c4699da3
Ivy Cagaanan <107980264+Lothany@users.noreply.github.com>
2023-10-14 22:41:32 +0800

    fixed config import

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_mysql_connector import MySQL
+from .config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY
+
+mysql = MySQL()
 
 def create_app():
     app = Flask(__name__)
@@ -7,6 +10,17 @@ def create_app():
 
     from .auth import auth
     from .view import view
+    
+    app.config.from_mapping(
+        SECRET_KEY=SECRET_KEY,
+        MYSQL_USER=DB_USERNAME,
+        MYSQL_PASSWORD=DB_PASSWORD,
+        MYSQL_DATABASE=DB_NAME,
+        MYSQL_HOST=DB_HOST,
+        #BOOTSTRAP_SERVE_LOCAL=BOOTSTRAP_SERVE_LOCAL
+    )
+    
+    mysql.init_app(app)
     
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(view, url_prefix='/')
