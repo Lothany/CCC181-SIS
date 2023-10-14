c3278baec2f83058b165413dacef848a10556108
Ivy Cagaanan <107980264+Lothany@users.noreply.github.com>
2023-10-14 19:13:06 +0800

    doesn;t work yet

--- database.sql
+++ database.sql
@@ -1,5 +1,8 @@
-DROP TABLE IF EXISTS colleges;
-CREATE TABLE IF NOT EXISTS colleges (
+DROP DATABASE IF EXISTS sis181;
+CREATE DATABASE sis181;
+USE sis181;
+
+CREATE TABLE colleges (
     collegeCode VARCHAR(255) NOT NULL,
     collegeName VARCHAR(255) NOT NULL,
     PRIMARY KEY (collegeCode)
