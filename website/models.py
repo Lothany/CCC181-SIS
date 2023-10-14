95b6f02d13fbee9b42008274ab22bf03153d84f6
Ivy Cagaanan <107980264+Lothany@users.noreply.github.com>
2023-10-15 00:13:20 +0800

    added college class

--- website/models.py
+++ website/models.py
@@ -1,1 +1,15 @@
 from website import mysql
+
+class Colleges(object):
+    def __init__(self, collegeCode=None, collegeName=None):
+        self.collegeCode = collegeCode
+        self.collegeName = collegeName
+        
+    def add(self):
+        cursor = mysql.connection.cursor()
+        
+        sql = f"INSERT INTO colleges(collegeCode, collegeName) \
+                VALUES('{self.collegeCode}', '{self.collegeName}')"
+            
+        cursor.execute(sql)
+        mysql.connection.commit()
