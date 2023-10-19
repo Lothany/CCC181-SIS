ecf27563a1efe51419d0a3f3feb5e159f22aaee7
Ivy Cagaanan <107980264+Lothany@users.noreply.github.com>
2023-10-20 01:47:17 +0800

    removed empty homepage

--- website/student/controller.py
+++ website/student/controller.py
@@ -4,9 +4,6 @@ import website.models as models
 student_bp = Blueprint('student', __name__)
 
 @student_bp.route('/')
-def home():
-    return render_template("home.html")
-
 @student_bp.route('/student')
 def view_student():
     students = models.Students.list()
