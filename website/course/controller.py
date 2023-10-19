0fd2d0a4277beea8384f2471d5ba0140a26229d7
Ivy Cagaanan <107980264+Lothany@users.noreply.github.com>
2023-10-19 15:54:14 +0800

    selected row for edit is reflected in edit_course page

--- website/course/controller.py
+++ website/course/controller.py
@@ -31,3 +31,25 @@ def add_course():
     
     colleges = models.Courses.list_colleges()
     return render_template("add_course.html", colleges=colleges)
+
+@course_bp.route('/course/edit', methods = ['GET', 'POST'])
+def edit_course():
+    if request.method == 'POST':
+        courseCode = request.form.get('courseCode')
+        courseName = request.form.get('courseName')
+        college = request.form.get('college')
+        trueCourse = request.args.get('courseCode')
+
+        course = models.Courses(courseCode, courseName, college)
+        exists = course.edit(trueCourse)
+        if exists == "duplicate":
+            flash('Course with the same code already exists!', category='error')
+        else:   
+            flash('Course edited successfully!', category='success')
+            return redirect('/course')
+
+    courseCode = request.args.get('courseCode')
+    courseName = request.args.get('courseName')
+    college = request.args.get('college')
+    collegeList = models.Courses.list_colleges()
+    return render_template("edit_course.html", courseCode=courseCode, courseName=courseName, college=college, collegeList = collegeList)
